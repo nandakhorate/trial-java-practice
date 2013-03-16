8f984c24ef66255286e79a1615b45cb9820117e1
syevle <syevle@gmail.com>
2013-03-16 21:32:59 +0530

    updated folder structure.

---
+++ basicSpringMVC3andHibernetProject/pune/src/main/java/com/ked/generic/GenericDAO.java
@@ -0,0 +1,60 @@
+package com.ked.generic;
+
+import java.util.List;
+
+
+public interface GenericDAO<K,T extends Kernel<K>> {
+	/**
+	 * Method to create and/or update a NTM Entity to the database.
+	 * This method does not commit. The caller should handle all transaction related nuances.
+	 * 
+	 * @param entity
+	 */
+	public void persist(T entity);
+    
+	/**
+	 * Method to delete a NTM Entity from the database.
+	 * This method does not commit. The caller should handle all transaction related nuances.
+	 * 
+	 * @param entity
+	 */
+    public void delete(T entity);
+    
+	/**
+	 * Method to read a NTM Entity from the database.
+	 * This method does not need an active transaction.
+	 * 
+	 * @param id
+	 * @return T
+	 */
+    public T findById(Long id);
+
+    /**
+     * Method to merge a detached object into the database.
+     * 
+     * @param entity
+     */
+    public void merge(T entity);
+	
+	/**
+	 * Method to read a NTM Entity from the database.
+	 * This method does not need an active transaction.
+	 * 
+	 * @param id
+	 * @return List<T>
+	 */
+    List<T> findAll();
+    
+    /**
+     * Find using a named query.
+     *
+     * @param queryName the name of the query
+     * @param params the query parameters
+     *
+     * @return the list of entities
+     */
+    List<T> findByNamedQuery(
+        final String queryName,
+        Object... params
+    );
+}
---
+++ basicSpringMVC3andHibernetProject/pune/src/main/java/com/ked/site/SiteDAO.java
@@ -0,0 +1,8 @@
+package com.ked.site;
+
+import com.ked.generic.GenericDAO;
+import com.ked.generic.Kernel;
+
+public interface SiteDAO<K,T extends Kernel<K>> extends GenericDAO<K,T> {
+
+}
