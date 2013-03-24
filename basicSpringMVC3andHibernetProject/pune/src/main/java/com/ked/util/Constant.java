2f2c8b6b48c770fe0f0f0070fbc8a01938512be8
syevle <syevle@gmail.com>
2013-03-24 09:21:42 +0530

    u

---
+++ basicSpringMVC3andHibernetProject/pune/src/main/java/com/ked/util/Constant.java
@@ -0,0 +1,15 @@
+package com.ked.util;
+
+public class Constant {
+	public static final String ROOTPATH = "/pune";
+	public static final String LIST = "/list";
+	public static final String ADD = "/add";
+	public static final String UPDATE = "/update";
+	public static final String REDIRECT = "redirect:";
+	public static final String ACTION = "/action";
+	public static final String DELETED_BY_ID = "/deleteById";
+	public static final String FIND = "/find";
+	public static final String SITE = "/site";
+	
+	
+}
